dd7b0a40366115f0e2fa18a6d92ede6258672711
Ruddy <honghao.2107@gmail.com>
2022-09-21 18:52:50 +0700

    my changes

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -4,14 +4,12 @@ import Image1 from "../../assets/avatar-1.svg";
 import Image3 from "../../assets/avatar-3.svg";
 import { themeContext } from "../../Context";
 import { useContext } from "react";
-// import Swiper core and required modules
-import { Pagination } from "swiper";
-
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 const data = [
   {
@@ -35,35 +33,36 @@ const data = [
 const Testimonials = () => {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
-  return (
-    <section className="testimonials container section">
-      <h2 style={{color: darkMode? 'white': ''}} className="section__title">Clients & Reviews</h2>
 
-      <Swiper
-        className="testimonials__container grid"
-        // install Swiper modules
-        modules={[Pagination]}
-        spaceBetween={50}
-        slidesPerView={1}
-        loop={true}
-        grabCursor={true}
-        pagination={{ clickable: true }}
-      >
-        {data.map(({ id, image, title, subtitle, comment }) => {
-          return (
-            <SwiperSlide className="testimonials__item" key={id}>
-              <div className="thumb">
-                <img src={image} alt="" />
-              </div>
-              <h3 style={{color: darkMode? 'blue': ''}} className="testimonials__title">{title}</h3>
-              <span style={{color: darkMode? 'white': ''}} className="subtitle">{subtitle}</span>
-              <div style={{color: darkMode? '#333': ''}} className="comment">{comment}</div>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
-    </section>
-  );
-};
+    return (
+      <section className="testimonials container section">
+        <h2 style={{color: darkMode? 'white': ''}} className="section__title">Clients & Reviews</h2>
+        <Swiper
+          className="testimonials__container grid"
+  
+          // install Swiper modules
+          modules={[Pagination]}
+          spaceBetween={50}
+          slidesPerView={1}
+          loop={true}
+          grabCursor={true}
+          pagination={{ clickable: true }}
+        >
+          {data.map(({ id, image, title, subtitle, comment }) => {
+            return (
+              <SwiperSlide className="testimonials__item" key={id}>
+                <div className="thumb">
+                  <img src={image} alt="" />
+                </div>
+                <h3 style={{color: darkMode? 'blue': ''}} className="testimonials__title">{title}</h3>
+                <span style={{color: darkMode? 'white': ''}} className="subtitle">{subtitle}</span>
+                <div style={{color: darkMode? '#333': ''}} className="comment">{comment}</div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </section>
+    );
+  }
 
 export default Testimonials;
